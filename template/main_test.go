b5e1cca9a354757709ab84dc401c86f71faaeea9
Lena Voytek <lena@voytek.dev>
2023-12-01 14:28:30 -0700

    Fix test template with multi-example

--- template/main_test.go
+++ template/main_test.go
@@ -4,13 +4,15 @@ import (
 	"testing"
 )
 
-var exampleInput = ``
+var part1Example = ``
 var part1Expected = ``
+
+var part2Example = ``
 var part2Expected = ``
 
 func Test_Part1(t *testing.T) {
 	t.Run("Part 1", func(t *testing.T) {
-		actual := Part1(exampleInput)
+		actual := Part1(part1Example)
 		if actual != part1Expected {
 			t.Errorf("Expected: %v, Actual: %v", part1Expected, actual)
 		}
@@ -19,8 +21,8 @@ func Test_Part1(t *testing.T) {
 
 func Test_Part2(t *testing.T) {
 	t.Run("Part 2", func(t *testing.T) {
-		actual := Part2(exampleInput)
-		if actual != part1Expected {
+		actual := Part2(part2Example)
+		if actual != part2Expected {
 			t.Errorf("Expected: %v, Actual: %v", part1Expected, actual)
 		}
 	})
