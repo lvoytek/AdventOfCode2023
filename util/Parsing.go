2d3d8f30a585ce60762857ad7957805f001ac871
Lena Voytek <lena@voytek.dev>
2023-12-01 23:15:21 -0700

    Ignore blank lines and add digit parser

--- util/Parsing.go
+++ util/Parsing.go
@@ -7,7 +7,9 @@ import (
 /* Return file contents as a list of strings containing each line */
 func ListofLines(input string) (lines []string) {
 	for _, line := range(strings.Split(input, "\n")) {
-		lines = append(lines, line)
+		if len(line) > 0 {
+			lines = append(lines, line)
+		}
 	}
 
 	return lines
@@ -16,8 +18,33 @@ func ListofLines(input string) (lines []string) {
 /* Return file contents as a matrix of runes */
 func MatrixOfCharacters(input string) (matrix [][]rune) {
 	for _, line := range(strings.Split(input, "\n")) {
-		characters := []rune(line)
-		matrix = append(matrix, characters)
+		if len(line) > 0 {
+			characters := []rune(line)
+			matrix = append(matrix, characters)
+		}
+	}
+
+	return matrix
+}
+
+/* Return file contents as a matrix of numeric digits with non-digits replaced */
+func MatrixOfDigits(input string, nonDigits int) (matrix [][]int) {
+	for _, line := range(strings.Split(input, "\n")) {
+		if len(line) == 0 {
+			continue
+		}
+
+		var lineOfDigits []int
+		for _, character := range(line) {
+			nextDigit := int(character) - int('0')
+			if nextDigit < 0 || nextDigit > 9 {
+				nextDigit = nonDigits
+			}
+
+			lineOfDigits = append(lineOfDigits, nextDigit)
+		}
+
+		matrix = append(matrix, lineOfDigits)
 	}
 
 	return matrix
