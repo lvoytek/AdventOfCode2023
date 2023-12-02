a040101fbd1e09779d0f0bbb589c3ad2056d27a1
Lena Voytek <lena@voytek.dev>
2023-12-01 23:15:57 -0700

    Finish day 2

---
+++ day2/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/lvoytek/AdventOfCode2023/util"
+)
+
+//go:embed input.txt
+var inputData string
+
+func main() {
+	var doPart int
+	flag.IntVar(&doPart, "p", 1, "Part number to run:")
+	flag.Parse()
+
+	if doPart != 2 {
+		fmt.Println("Part 1 Output:", Part1(inputData))
+	}
+
+	if doPart != 1 {
+		fmt.Println("Part 2 Output:", Part2(inputData))
+	}
+}
+
+type Pull struct {
+	reds int
+	greens int
+	blues int
+}
+
+type Game struct {
+	id int
+	pulls []Pull
+}
+
+func extractGame(line string) Game {
+	re := regexp.MustCompile(`Game (\d+):`)
+	gameNumber, _ := strconv.Atoi(re.FindStringSubmatch(line)[1])
+
+	re = regexp.MustCompile(`(\d+)\s+(red|green|blue)`)
+	var pulls []Pull
+
+	for _, pullString := range(strings.Split(line, ";")) {
+		var nextPull Pull
+		colorSet := re.FindAllString(pullString, -1)
+
+		for _, color := range(colorSet) {
+			if strings.Contains(color, "red") {
+				nextPull.reds = util.GetFirstIntInString(color)
+			} else if strings.Contains(color, "green") {
+				nextPull.greens = util.GetFirstIntInString(color)
+			} else {
+				nextPull.blues = util.GetFirstIntInString(color)
+			}
+		}
+
+		pulls = append(pulls, nextPull)
+	}
+
+	return Game {
+		id: gameNumber,
+		pulls: pulls,
+	}
+}
+
+func Part1(input string) string {
+	lines := util.ListofLines(input)
+
+	const maxRed = 12
+	const maxGreen = 13
+	const maxBlue = 14
+
+	gameSum := 0
+
+	for _, line := range(lines) {
+		nextGame := extractGame(line)
+
+		gameIsValid := true
+
+		for _, pull := range(nextGame.pulls) {
+			if pull.reds > maxRed || pull.greens > maxGreen || pull.blues > maxBlue {
+				gameIsValid = false
+				break
+			}
+		}
+
+		if gameIsValid {
+			gameSum += nextGame.id
+		}
+
+	}
+
+	return fmt.Sprint(gameSum)
+}
+
+func Part2(input string) string {
+	lines := util.ListofLines(input)
+
+	sumOfPower := 0
+
+	for _, line := range(lines) {
+		nextGame := extractGame(line)
+
+		maxRed := 0
+		maxGreen := 0
+		maxBlue := 0
+
+		for _, pull := range(nextGame.pulls) {
+			if pull.reds > maxRed {
+				maxRed = pull.reds
+			}
+
+			if pull.greens > maxGreen {
+				maxGreen = pull.greens
+			}
+
+			if pull.blues > maxBlue {
+				maxBlue = pull.blues
+			}
+		}
+
+		sumOfPower += maxRed * maxGreen * maxBlue
+	}
+
+	return fmt.Sprint(sumOfPower)
+}
---
+++ day2/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+var part1Example = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+var part1Expected = `8`
+
+var part2Example = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+var part2Expected = `2286`
+
+func Test_Part1(t *testing.T) {
+	t.Run("Part 1", func(t *testing.T) {
+		actual := Part1(part1Example)
+		if actual != part1Expected {
+			t.Errorf("Expected: %v, Actual: %v", part1Expected, actual)
+		}
+	})
+}
+
+func Test_Part2(t *testing.T) {
+	t.Run("Part 2", func(t *testing.T) {
+		actual := Part2(part2Example)
+		if actual != part2Expected {
+			t.Errorf("Expected: %v, Actual: %v", part2Expected, actual)
+		}
+	})
+}
