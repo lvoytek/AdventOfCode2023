1591c73adbd591c6ca624db44d3f0348a9d9ac09
Lena Voytek <lena@voytek.dev>
2023-12-03 22:59:23 -0700

    Add function to get all ints in string

--- util/Convert.go
+++ util/Convert.go
@@ -11,3 +11,18 @@ func GetFirstIntInString(input string) int {
 	result, _ := strconv.Atoi(intRe.FindString(input))
 	return result
 }
+
+/* Get all positive integers in a string */
+func GetAllIntsInString(input string) []int {
+	intRe := regexp.MustCompile(`(\d+)`)
+	resultStrings := intRe.FindAllString(input, -1)
+
+	var result []int
+
+	for _, resultString := range(resultStrings) {
+		nextResult, _ := strconv.Atoi(resultString)
+		result = append(result, nextResult)
+	}
+
+	return result
+}
