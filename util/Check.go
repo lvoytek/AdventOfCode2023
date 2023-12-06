0fb6c0650490a57dfbcd46abad4bd63181593d72
Lena Voytek <lena@voytek.dev>
2023-12-05 20:44:00 -0700

    Add max and min for ints

--- util/Check.go
+++ util/Check.go
@@ -3,3 +3,17 @@ package util
 func RuneIsNumber(input rune) bool {
 	return int(input) >= int('0') && int(input) <= int('9')
 }
+
+func MaxInt(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func MinInt(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
